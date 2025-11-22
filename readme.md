5a8b377bcd4180a7f627926d23994df6b04b1a18
MyUsername <development.for.code@gmail.com>
2025-11-22 09:55:08 +0100

    js-our-team

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Traccia   
+
+- Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata in cui mostrare una card per ciascun componente.
+
+Partite con il minimo HTML e CSS necessari.
+
+Trovate l’array del team all’interno della cartella in allegato.
---
+++ script.js
@@ -0,0 +1,78 @@
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png"
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png"
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png"
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png"
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png"
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png"
+  }
+];
+
+
+function cardAziendaComp() {
+  
+  const listElement = document.getElementById("flex-container");
+  let listHTML = "";
+  for (let i = 0; i < teamMembers.length; i++) {
+  const team = teamMembers[i];
+  
+
+
+  const card = `<div class="card" style="width: 18rem;">
+    <img src="${team.img}" class="card-img-top" alt="${team.name}">
+      <div class="card-body">
+        <h5 class="card-title">${team.name}</h5>
+        <p class="card-text">${team.role}</p>
+        <p class="mail">${team.email}</p>
+        <p></p>
+      </div>
+  </div>`;
+
+
+
+listHTML += card;
+
+}
+
+
+listElement.innerHTML = listHTML;
+  
+
+
+}
+
+
+cardAziendaComp();
+
+
+
+
